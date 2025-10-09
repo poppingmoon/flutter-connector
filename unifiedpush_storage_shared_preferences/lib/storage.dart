f4b2c5191793687dfb28ff0807499d52a0bb729b
sim <git@sgougeon.fr>
2025-10-09 17:37:14 +0200

    Implement UnifiedPushStorage with SharedPreferences

--- unifiedpush_storage_shared_preferences/lib/storage.dart
+++ unifiedpush_storage_shared_preferences/lib/storage.dart
@@ -1,50 +1,120 @@
 import 'dart:async';
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:unifiedpush_storage_interface/distributor_storage.dart';
+import 'package:unifiedpush_storage_interface/keys_storage.dart';
+import 'package:unifiedpush_storage_interface/registrations_storage.dart';
 import 'package:unifiedpush_storage_interface/storage.dart';
 
 class UnifiedPushStorageSharedPreferences extends UnifiedPushStorage {
-  FutureOr<SharedPreferences> _prefs() async {
-    return await SharedPreferences.getInstance();
-  }
-
   @override
   FutureOr<void> init() async {
     // Nothing to do
   }
 
   @override
-  FutureOr<void> setString(String key, String value) async {
-    (await _prefs()).setString(key, value);
+  DistributorStorage get distrib => DistributorStorageSharedPrefs();
+
+  @override
+  KeysStorage get keys => KeysStorageSharedPrefs();
+
+  @override
+  RegistrationsStorage get registrations => RegistrationsStorageSharedPrefs();
+}
+
+class DistributorStorageSharedPrefs extends DistributorStorage {
+  static const String _ACK = "unifiedpush.distributor.ack";
+  static const String _NAME = "unifiedpush.distributor.name";
+
+  @override
+  FutureOr<void> ack() {
+    return SharedPreferencesAsync().setBool(_ACK, true);
   }
 
   @override
-  FutureOr<String?> getString(String key) async {
-    return (await _prefs()).getString(key);
+  FutureOr<String?> get() {
+    return SharedPreferencesAsync().getString(_NAME);
   }
 
   @override
-  FutureOr<void> setSecret(String key, String value) {
-    return setString(key, value);
+  FutureOr<void> remove() async {
+    await SharedPreferencesAsync().remove(_ACK);
+    return SharedPreferencesAsync().remove(_NAME);
   }
 
   @override
-  FutureOr<String?> getSecret(String key) {
-    return getString(key);
+  FutureOr<void> set(String distributor) async {
+    final current = get();
+    if (current != distributor) {
+      await SharedPreferencesAsync().remove(_ACK);
+    }
+    return SharedPreferencesAsync().setString(_NAME, distributor);
+  }
+}
+
+class KeysStorageSharedPrefs extends KeysStorage {
+  static const String _KEY = "unifiedpush.key";
+  @override
+  FutureOr<String?> get(String instance) {
+    return SharedPreferencesAsync().getString("$_KEY.$instance");
   }
 
   @override
-  FutureOr<void> setBool(String key, bool value) async {
-    (await _prefs()).setBool(key, value);
+  FutureOr<void> remove(String instance) {
+    return SharedPreferencesAsync().remove("$_KEY.$instance");
   }
 
   @override
-  FutureOr<bool?> getBool(String key) async {
-    return (await _prefs()).getBool(key);
+  FutureOr<void> set(String instance, String serializedKey) {
+    return SharedPreferencesAsync().setString("$_KEY.$instance", serializedKey);
+  }
+}
+
+class RegistrationsStorageSharedPrefs extends RegistrationsStorage {
+  /// To get instance from token
+  static const String _INSTANCE_FOR = "unifiedpush.instance_for";
+  /// To get token from instance
+  static const String _TOKEN_FOR = "unifiedpush.token_for";
+
+  @override
+  FutureOr<TokenInstance?> getFromInstance(String instance) async {
+    final token = await SharedPreferencesAsync().getString("$_TOKEN_FOR.$instance");
+    if (token == null) return null;
+    return TokenInstance(token, instance);
   }
 
   @override
-  FutureOr<void> remove(String key) async {
-    (await _prefs()).remove(key);
+  FutureOr<TokenInstance?> getFromToken(String token) async {
+    final instance = await SharedPreferencesAsync().getString("$_INSTANCE_FOR.$token");
+    if (instance == null) return null;
+    return TokenInstance(token, instance);
+  }
+
+  @override
+  FutureOr<bool> remove(String instance) async {
+    final token = await SharedPreferencesAsync().getString("$_TOKEN_FOR.$instance");
+    if (token != null) {
+      await SharedPreferencesAsync().remove("$_INSTANCE_FOR.$token");
+    }
+    await SharedPreferencesAsync().remove("$_TOKEN_FOR.$instance");
+    return (await SharedPreferencesAsync().getKeys())
+        .any((it) => it.startsWith(_INSTANCE_FOR));
+  }
+
+  @override
+  FutureOr<void> removeAll() async {
+    (await SharedPreferencesAsync().getKeys())
+        .forEach((key) async {
+          if (key.startsWith(_INSTANCE_FOR) || key.startsWith(_TOKEN_FOR)) {
+            await SharedPreferencesAsync().remove(key);
+          }
+        }
+    );
+  }
+
+  @override
+  FutureOr<void> save(TokenInstance token) async {
+    await SharedPreferencesAsync().setString("$_INSTANCE_FOR.${token.token}", token.instance);
+    return SharedPreferencesAsync().setString("$_TOKEN_FOR.${token.instance}", token.token);
   }
 }
